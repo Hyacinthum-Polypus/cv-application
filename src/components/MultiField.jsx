8930d02c32cbc52c1e080562770dbe48c9601e0e
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2025-01-24 22:29:52 +1100

    Group each group of fields in a multifield

--- src/components/MultiField.jsx
+++ src/components/MultiField.jsx
@@ -8,7 +8,13 @@ function MultiField({status, fieldName, children}) {
 
     return (
         <div>
-            {fields}
+            {fields.map((group, index) => {
+                return(
+                    <div key={index} className="fieldGroup">
+                        {group}
+                    </div>
+                )
+            })}
             <button onClick={() => setFields([...fields, [React.Children.map(enhancedChildren, child => React.cloneElement(child))]])}>Add</button>
             <button onClick={() => {if(fields.length > 0) setFields(fields.slice(0, -1))}}>Remove</button>
         </div>
