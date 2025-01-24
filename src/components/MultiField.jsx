ae458fe027ce0039f875b494203259dcab2d53bb
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2025-01-24 22:43:51 +1100

    Fix status not being fed to fields

--- src/components/MultiField.jsx
+++ src/components/MultiField.jsx
@@ -1,22 +1,22 @@
 import React,{useState} from 'react'
 import '../styles/MultiField.css'
 
-function MultiField({status, fieldName, children}) {
+function MultiField({status, children}) {
 
-    const enhancedChildren = React.Children.map(children, child => React.cloneElement(child, {"status": status}));
+    //const enhancedChildren = React.Children.map(children, child => React.cloneElement(child, {"status": status}));
 
-    const [fields, setFields] = useState([[React.Children.map(enhancedChildren, child => React.cloneElement(child))]]);
+    const [fields, setFields] = useState([[React.Children.map(children, child => React.cloneElement(child))]]);
 
     return (
         <div>
             {fields.map((group, index) => {
                 return(
                     <div key={index} className="field-group">
-                        {group}
+                        {React.Children.map(group, child => React.cloneElement(child, {'status': status}))}
                     </div>
                 )
             })}
-            <button onClick={() => setFields([...fields, [React.Children.map(enhancedChildren, child => React.cloneElement(child))]])}>Add</button>
+            <button onClick={() => setFields([...fields, [React.Children.map(children, child => React.cloneElement(child))]])}>Add</button>
             <button onClick={() => {if(fields.length > 0) setFields(fields.slice(0, -1))}}>Remove</button>
         </div>
     )
